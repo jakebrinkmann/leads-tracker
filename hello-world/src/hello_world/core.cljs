ab3d5cfbb7cd2b47a28624c711436b3690ceff4b
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-01-15 15:42:32 -0600

    rev(hello-world): go back to simplest example
    * Go back to square one, to have a working setup

--- hello-world/src/hello_world/core.cljs
+++ hello-world/src/hello_world/core.cljs
@@ -1,12 +1,12 @@
 (ns hello-world.core
   (:require react-dom))
 
-(defn component []
-  [:div
-    "Hello, world!"])
+; (defn component []
+;   [:div
+;     "Hello, world!"])
 
 (println "hi!")           ; prints "hi!" to the console
 
-(.render js/ReactDOM
-  (.createElement js/React "h2" nil "Hello, React!")
-  (.getElementById js/document "app"))
+; (.render js/ReactDOM
+;   (.createElement js/React "h2" nil "Hello, React!")
+;   (.getElementById js/document "app"))
